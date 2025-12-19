5b5eb7451257f7b8b68ba668e92e94305698da0f
AIMagicCrafter <nissu342@gmail.com>
2025-12-20 00:18:33 +0530

    Set up Playwright config and base test structure

---
+++ playwright.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 30 * 1000,
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [["html", { open: "never" }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL: "https://demo.playwright.dev/todomvc",
+    headless: false,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+  ],
+});
